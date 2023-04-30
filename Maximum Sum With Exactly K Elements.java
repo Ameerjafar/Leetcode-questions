8a30f86dd65701f7e5cabdd402181abaf39ab852
Ameerjafar <105500361+Ameerjafar@users.noreply.github.com>
2023-04-30 09:20:22 +0530

    Create Maximum Sum With Exactly K Elements.java

---
+++ Maximum Sum With Exactly K Elements.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int helper(int[] nums, int k){
+        Arrays.sort(nums);
+        int sum = 0;
+        for(int i =0; i < k; i++){
+            sum += nums[nums.length - 1];
+            nums[nums.length - 1] = nums[nums.length - 1] + 1;
+        }
+        return sum;
+    }
+    public int maximizeSum(int[] nums, int k) {
+        return helper(nums, k);
+    }
+}
